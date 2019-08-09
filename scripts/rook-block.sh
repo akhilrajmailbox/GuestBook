4a2d67812ffe4c926deec39e2cae70057535e30a
Akhil Raj <akhilrajmailbox@gmail.com>
2019-08-09 14:25:41 +0530

    updated

--- scripts/rook-block.sh
+++ scripts/rook-block.sh
@@ -4,9 +4,11 @@
 ################################################
 function rook_block() {
     kubectl apply -f ../rook-block/rook-operator.yaml
+    sleep 30
     kubectl apply -f ../rook-block/rook-cluster.yaml
+    sleep 30
     kubectl apply -f ../rook-block/rook-storageclass.yaml
-
+    sleep 30
     ## making rook-block available in kube-system namespace, 
     kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/creat/d;/namespace/d" | kubectl -n kube-system apply -f -
 }
